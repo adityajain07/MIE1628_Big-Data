f4e205658e46450d43580c10f8397093b17c656c
Aditya Jain <aditya14129@iiitd.ac.in>
2023-02-13 22:02:57 -0500

    generate predictions for the two users ids

--- Assignment2/partB5.py
+++ Assignment2/partB5.py
@@ -1,19 +1,30 @@
 from pyspark.sql import SparkSession
-from pyspark.ml.recommendation import ALSModel
 from pyspark.ml.tuning import CrossValidatorModel
+from pyspark.sql import Row
+from pyspark.ml.recommendation import ALS
+from pyspark.sql.functions import col
 
 
 # start spark session
 spark = SparkSession.builder.appName('rec-eval').getOrCreate()
 
+# load data
+rdd_orig   = spark.read.option("header", True).csv('movies.csv').rdd
+ratingsRDD = rdd_orig.map(lambda p: Row(userId=int(p[2]), movieId=int(p[0]),
+                                     rating=int(p[1])))
+ratings    = spark.createDataFrame(ratingsRDD)
+
 # load trained model
+als        = ALS(userCol="userId", itemCol="movieId", ratingCol="rating", coldStartStrategy="drop")
 model_path = 'rec-model-v01/'
-model = CrossValidatorModel.read().load(model_path)
+model      = CrossValidatorModel.read().load(model_path)
 
-# print(model.bestModel._java_obj.parent().getMaxIter())
+# chose the users required for prediction
+users = ratings.select(als.getUserCol()).distinct()
+users = users.filter((col("userId") == 10) | (col("userId") == 14))
 
-print(model.bestModel._java_obj.parent().getRank())
-print(model.bestModel.params)
+movie_rec = model.bestModel.recommendForUserSubset(users, 15)
 
+print(movie_rec.show())
 
 
